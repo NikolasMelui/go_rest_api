d787aa9d3d2d8eab1f482172e25069749371dd74
nikolasmelui <slak@samaradom.ru>
2020-05-12 23:47:29 +0400

    - Add config parse to the main file - Remove BurntSushi from inderect

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
 
+	"github.com/BurntSushi/toml"
 	"github.com/nikolasmelui/go_rest_api/internal/app/apiserver"
 )
 
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "config/apiserver.toml", "path to the config file")
+}
+
 func main() {
-	s := apiserver.New()
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/nikolasmelui/go_rest_api
 
 go 1.14
 
-require github.com/BurntSushi/toml v0.3.1 // indirect
+require github.com/BurntSushi/toml v0.3.1
