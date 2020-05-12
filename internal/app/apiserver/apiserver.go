c7cac942cebf8b5950c1700472a78a21925f6976
nikolasmelui <slak@samaradom.ru>
2020-05-13 00:21:11 +0400

    Add router logic and 3 handlers

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,16 +1,24 @@
 package apiserver
 
-import "github.com/sirupsen/logrus"
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
 
 type APIServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -19,9 +27,11 @@ func (s *APIServer) Start() error {
 		return err
 	}
 
+	s.configureRouter()
+
 	s.logger.Info("starting the server")
 
-	return nil
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 func (s *APIServer) configureLogger() error {
@@ -33,4 +43,28 @@ func (s *APIServer) configureLogger() error {
 	s.logger.SetLevel(level)
 
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/", s.handleMain())
+	s.router.HandleFunc("/first", s.handleFirst())
+	s.router.HandleFunc("/second", s.handleSecond())
+}
+
+func (s *APIServer) handleMain() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Test the main router")
+	}
+}
+
+func (s *APIServer) handleFirst() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "This is the first handler")
+	}
+}
+
+func (s *APIServer) handleSecond() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "This is the second handler")
+	}
 }
